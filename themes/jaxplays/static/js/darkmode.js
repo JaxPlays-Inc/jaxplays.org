f57ead67b028d550f1450416ddbe9244e3f61876
Ray Hollister <github@rayhollister.com>
2023-12-19 16:48:33 -0500

    Updated darkmode.js to fix PWA issue https://jaxplays.canny.io/report/p/dark-mode-inconsistency-on-ios-app

--- themes/jaxplays/static/js/darkmode.js
+++ themes/jaxplays/static/js/darkmode.js
@@ -1,19 +1,27 @@
-// Checking LocalStorage and user's preference on page load
-var darkModeEnabled = localStorage.getItem('darkMode') === 'enabled' || 
-                      (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && 
-                       localStorage.getItem('darkMode') !== 'disabled');
+function applyDarkMode() {
+  var darkModeEnabled = localStorage.getItem('darkMode') === 'enabled' || 
+                        (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && 
+                         localStorage.getItem('darkMode') !== 'disabled');
 
-if (darkModeEnabled) {
-  document.body.classList.add('dark-mode');
-}
-
-document.getElementById('dark-mode-toggle').addEventListener('click', function () {
-  darkModeEnabled = !darkModeEnabled;
   if (darkModeEnabled) {
     document.body.classList.add('dark-mode');
-    localStorage.setItem('darkMode', 'enabled'); // Saving to LocalStorage
   } else {
     document.body.classList.remove('dark-mode');
-    localStorage.setItem('darkMode', 'disabled'); // Setting to disabled in LocalStorage
+  }
+}
+
+// Call applyDarkMode on page load
+applyDarkMode();
+
+// Event listener for the dark mode toggle button
+document.getElementById('dark-mode-toggle').addEventListener('click', function () {
+  var darkModeEnabled = !document.body.classList.contains('dark-mode');
+  
+  if (darkModeEnabled) {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'disabled');
   }
 });
